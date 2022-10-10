50c2eee1661e6b0cb78c81e1c7862dded09cfbc0
w3arthur <w3arthur@gmail.com>
2022-10-10 13:24:49 +0300

    web socket

--- ServerSide_ClientSide_WebSocket_NodejsExpress/server.js
+++ ServerSide_ClientSide_WebSocket_NodejsExpress/server.js
@@ -1,48 +1,44 @@
-//to delete not needed installation
-const express = require("express");
-const app = express();
-//const router = express.Router();  
-//examples here https://stackoverflow.com/questions/22429744/how-to-setup-route-for-websocket-server-in-express
-const { createServer } = require("http");
-const WebSocket = require('ws');
-const PORT = 5000;
 const path = require("path");
+const PORT = 5000;
+const WS_PATH = "/ws";
 const public_folder = 'public_folder';
 const getFile = (fileInPublicFolder = '') => path.join(__dirname, public_folder, fileInPublicFolder);
 
-
-
-
+//to delete not needed installation
+const express = require("express");
+const WebSocket = require('ws');
+const { createServer } = require("http");
+const app = express();
 const server = createServer(app);
+const wsServer = new WebSocket.Server({ server , path: WS_PATH }) //{port: PORT}
+//const router = express.Router();  
+//examples here https://stackoverflow.com/questions/22429744/how-to-setup-route-for-websocket-server-in-express
 
 
-
-app.use(require("cookie-parser")());
-app.use(express.json({ extended: false }));
-app.use(express.urlencoded());
-
-app.get("/", async (req, res) => res.status(200).sendFile(getFile("index.html")) );
-app.use("/", express.static(getFile()));  //global public folder
-app.route("*").all((req, res) => res.status(404).send("fail" + JSON.stringify(req.path)) );
-
-
-server.listen(PORT, () => console.log(`Listening on port ${PORT}, Express + WS`));
-
-
-
-const wsServer = new WebSocket.Server({ server , path: "/ws" }) //{port: PORT}
 wsServer.on('connection', (socket) => {
     console.log("A client just connected");
     socket.on('message', (msg) => {
         console.log("Desktop application is connected; message: " + msg);
         
         wsServer.clients.forEach((client) => {client.send(msg + " Return :) ")});
-
+        //specific client
     });
     socket.on('close', () => {
         console.log('A client just disconnected.')
     });
-
 } );
 
-console.log( (new Date()) + "Server is listening on port " + PORT );
+app.use(require("cookie-parser")());
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded());
+
+app.route("/").get(async (req, res) => res.status(200).sendFile(getFile("index.html")) );
+app.use("/", express.static(getFile()));  //global public folder
+app.route("/arduino").get(async (req, res) => {
+
+
+} );
+app.route("*").all((req, res) => res.status(404).send("fail" + JSON.stringify(req.path)) );
+
+
+server.listen(PORT, () => console.log(`${(new Date().toISOString())} Server is listening on port ${PORT}, (Express + WS)`));
