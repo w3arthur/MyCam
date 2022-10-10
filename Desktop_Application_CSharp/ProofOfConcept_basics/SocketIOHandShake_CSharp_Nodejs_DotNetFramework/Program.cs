9621a626f76e1d36220dbdf2ea25985049ae33b6
w3arthur <w3arthur@gmail.com>
2022-10-10 15:30:57 +0300

    C#

--- Desktop_Application_CSharp/ProofOfConcept_basics/SocketIOHandShake_CSharp_Nodejs_DotNetFramework/Program.cs
+++ Desktop_Application_CSharp/ProofOfConcept_basics/SocketIOHandShake_CSharp_Nodejs_DotNetFramework/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
-using WebSocketSharp;   // NuGet\Install-Package WebSocketSharp -Version 1.0.3-rc11 // https://github.com/sta/websocket-sharp
-using WebSocket = WebSocketSharp.WebSocket; //try another package or use .Net Framework
+using WebSocketSharp;
 
+// change to C# (.NET)
+// NuGet\Install-Package WebSocketSharp -Version 1.0.3-rc11
+// https://github.com/sta/websocket-sharp
 // Add issue if no server
 // if server turned off or delay to recheck if it reconnected or error message
 // close server> client if app off
@@ -15,25 +16,25 @@ namespace SocketIOHandShake
 {
     internal class Program
     {
+
         public static WebSocket ws;
         static void Main(string[] args)
         {
             Console.WriteLine("Web Socket");
             // Create an instance of WS Client
             //using
-            using (ws = new WebSocket("ws://127.0.0.1:5000/ws_arduino"))
-            {    //localhost
-                ws.Connect();
-                ws.Ping();
-                ws.Send("desktopApplication");
-                ws.OnMessage += Ws_OnMessage!; // += add new event handler
+            ws = new WebSocket("ws://127.0.0.1:5000/ws_arduino");   //localhost
+            ws.Connect();
+            ws.Ping();
+            ws.Send("desktopApplication");
+            ws.OnMessage += Ws_OnMessage; // += add new event handler
 
 
-                Console.WriteLine("Please leave the program running!"); 
-                Console.ReadKey();
-            }
+            Console.WriteLine("Please leave the program running!");
+            Console.ReadKey();
+
         }
-        ~Program() { ws?.Close(); }
+        ~Program() { ws.Close(); }
 
         private static void Ws_OnMessage(object sender, MessageEventArgs e) //Who send, Contain Arguments
         {
