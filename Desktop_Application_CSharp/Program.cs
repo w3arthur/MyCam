e57648e26fe6ed56d5a74310a64fcb6ff55c0b41
w3arthur <w3arthur@gmail.com>
2022-10-10 20:10:37 +0300

    update

---
+++ Desktop_Application_CSharp/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using WebSocketSharp;   // NuGet\Install-Package WebSocketSharp -Version 1.0.3-rc11 // https://github.com/sta/websocket-sharp
+using WebSocket = WebSocketSharp.WebSocket; //try another package or use .Net Framework
+using ErrorEventArgs = WebSocketSharp.ErrorEventArgs;
+
+
+// Add issue if no server
+// if server turned off or delay to recheck if it reconnected or error message
+// close server> client if app off
+// Create an instance of WS Client
+//ping to server
+// the port is closed issue (turn off the arduino)
+// on error reset after time...
+
+
+namespace SocketIOHandShake
+{
+    internal class Program
+    {
+
+        public static readonly string WebSocketLocation = "ws://127.0.0.1:5000/ws_arduino";  //localhost
+        public static readonly string ApiWebSocketLocation = "http://localhost:5000/api/arduino";
+        public static readonly string ArduinoSerialTestValue = "0";
+        public static readonly string WebSocketApprovalMessage = "desktopApplication";
+        public static WebSocket? ws;
+        public static SerialPort? myport;
+        public static readonly string[] ports = SerialPort.GetPortNames();
+
+
+        public static CancellationTokenSource sourceToken = new CancellationTokenSource();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Web Socket");
+            try
+            {
+                using (myport = new SerialPort())
+                using (ws = new WebSocket(WebSocketLocation))
+                {
+                    //serial port
+                    myport.BaudRate = 9600;
+                    myport.PortName = ports[ports.Length - 1]; //myport.PortName = "COM3";  //Please fix!
+
+                   
+
+
+                    myport.Open();
+                    Console.WriteLine("please enter 1: ");
+
+                    ws.OnClose += Ws_OnClose; ;
+
+                    ws.Connect();
+                       //mess on error
+                    if (!ws.Ping()) { throw new Exception("There is websocket server issue, selected server: " + WebSocketLocation); }
+                    ws.Send(WebSocketApprovalMessage);
+                    ws.OnMessage += Ws_OnMessage!; // += add new event handler
+
+                    
+
+                    Console.WriteLine("Please leave the program running!");
+                    Console.WriteLine("Sure the Arduino connected first!, selected port: " + myport.PortName);
+                    Console.WriteLine("check functionality with " + "http://localhost:5000/api/arduino");
+                    Task.Run(() => { Task.Delay(500); ArduinoLed("0"); });
+          
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)  { Console.Clear(); Console.WriteLine("! "+ex.Message); }
+
+
+        }
+
+        private static void Ws_OnClose(object? sender, CloseEventArgs e)
+        {
+            Console.Clear();    //mess on error
+            if (!(ws?.Ping() ?? false)) { Console.WriteLine("There is websocket server issue, selected server: " + WebSocketLocation);}
+        }
+
+
+        ~Program() { ws?.Close(); myport?.Dispose(); }
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            try 
+            { 
+                Console.WriteLine(e.Data); //"Received from the server " 
+                ArduinoLed(e.Data);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        } //Who send, Contain Arguments
+
+
+
+        private static void ArduinoLed(string input)
+        {
+            try
+            {
+
+                sourceToken.CancelAfter(5000);
+                int intTry = 0;
+                if (input.Contains(WebSocketApprovalMessage)) return;
+                if (!int.TryParse(input, out intTry) ) throw new Exception("Input is not an int, the input " + input); ;
+                if (!myport!.IsOpen) throw new Exception("Arduino port is closed!, port:" + myport.PortName); ;
+                myport!.WriteLine(input);
+                //Console.WriteLine(intTemp);
+                Console.Write(myport.ReadLine());
+                Console.Write(myport.ReadLine());
+                Console.WriteLine();
+            }
+            catch (Exception ex) { throw new Exception( "Arduino Issue, Please Reset the program, issue:" + ex.Message);  }
+
+        }
+
+
+
+
+
+    }
+}
--- ServerSide_ClientSide_WebSocket_NodejsExpress/server.js
+++ ServerSide_ClientSide_WebSocket_NodejsExpress/server.js
@@ -1,9 +1,18 @@
-const path = require("path");
+const path = require('path');
 const PORT = 5000;
-const WS_PATH_1 = "/ws_arduino";
+const WS_PATH_1 = '/ws_arduino';
+const WS_PATH_1_APPROVAL_MESSAGE = 'desktopApplication';    // if receive another one will not include to ws clients list
+const API_TURNON_LED_RETURN = '1';
 const public_folder = 'public_folder';
 const getFile = (fileInPublicFolder = '') => path.join(__dirname, public_folder, fileInPublicFolder);
-const broadcast = (clients, message) => clients.forEach((client) => { if (client.readyState === WebSocket.OPEN)  client.send(message); });
+const broadcast = (clients, message) => {
+    try{
+        if(!clients || clients.size == 0) throw new Error();
+        clients.forEach((client) => { if (client.readyState === WebSocket.OPEN)  client.send(message); });
+        return true;
+    } catch {return false;}
+}
+
 
 //to delete not needed installation
 const express = require("express");
@@ -11,7 +20,7 @@ const WebSocket = require('ws');
 const { createServer } = require("http");
 const app = express();
 const server = createServer(app);
-const wsServer = new WebSocket.Server({ server , path: WS_PATH_1 }) //{port: PORT}
+const wsServer1 = new WebSocket.Server({ server , path: WS_PATH_1 }) //{port: PORT}
 //const router = express.Router();  
 //examples here https://stackoverflow.com/questions/22429744/how-to-setup-route-for-websocket-server-in-express
 app.use(require("cookie-parser")());
@@ -19,16 +28,17 @@ app.use(express.json({ extended: false }));
 app.use(express.urlencoded());
 
 
-wsServer.on('connection', (socket) => {
-    console.log("A client just connected, total clients: " + wsServer.clients.size);
+wsServer1.on('connection', (socket) => {
+    console.log("A client just connected, total clients: " + wsServer1.clients.size);
     socket.on('message', (msg) => {
-        console.log("Desktop application is connected; message: " + msg);
-        wsServer.clients.forEach((client) => {client.send("Message received, " + msg + "")});
-        //specific client
-        app.locals.clients = wsServer.clients;  // local data set
+        console.log("Desktop application is sending message: " + msg);
+        if(msg == WS_PATH_1_APPROVAL_MESSAGE) app.locals.clients1 = wsServer1.clients;  //specific client
+        wsServer1.clients.forEach((client) => {client.send("Message received by server, " + msg + "")});
     });
     socket.on('close', () => {
-        console.log('A client just disconnected.')
+        app.locals.clients1 = wsServer1.clients;
+        console.log('the desktop application just disconnected!, client still connected: ' + wsServer1.clients.size)
+        console.log('Please restart the desktop application ')
     });
 } );
 
@@ -37,10 +47,9 @@ app.route("/").get(async (req, res) => res.status(200).sendFile(getFile("index.h
 app.use("/", express.static(getFile()));  //global public folder
 
 app.route("/api/arduino").get(async (req, res) => {
-    console.log('arduino requested');
-    res.status(200).send('arduino requested');
-
-    broadcast(req.app.locals.clients, "/api/arduino requested, turn on the lamp")
+    const result = broadcast(req.app.locals.clients1, API_TURNON_LED_RETURN);   ///api/arduino requested, turn on the lamp
+    res.status(200).send('arduino requested ' + (result ? 'sent' : 'not sent!'));
+    console.log('arduino requested ' + (result ? 'sent' : 'not sent!'));
 } );
 
 app.route("*").all((req, res) => res.status(404).send("fail " + req.path) );
