e85c116fa74868694e11d06350862af8b8f6619c
w3arthur <w3arthur@gmail.com>
2022-10-13 11:00:33 +0300

    Web Streaming

--- ServerSide_ClientSide_WebSocket_NodejsExpress/server.js
+++ ServerSide_ClientSide_WebSocket_NodejsExpress/server.js
@@ -1,18 +1,16 @@
 const path = require('path');
-const PORT = 5000;
+const PORT = 8000;
 const WS_PATH_1 = '/ws_arduino';
 const WS_PATH_1_APPROVAL_MESSAGE = 'desktopApplication';    // if receive another one will not include to ws clients list
 const API_TURNON_LED_RETURN = '1';
 const public_folder = 'public_folder';
 const getFile = (fileInPublicFolder = '') => path.join(__dirname, public_folder, fileInPublicFolder);
 const broadcast = (clients, message) => {
-    try{
-        if(!clients || clients.size == 0) throw new Error();
+    try{ if(!clients || clients.size == 0) throw new Error();
         clients.forEach((client) => { if (client.readyState === WebSocket.OPEN)  client.send(message); });
         return true;
     } catch {return false;}
 }
-
 
 //to delete not needed installation
 const express = require("express");
@@ -28,6 +26,35 @@ app.use(express.json({ extended: false }));
 app.use(express.urlencoded());
 
 
+
+//http:
+app.use("/", express.static(getFile()));  //global public folder
+app.route("/").get(async (req, res) => res.status(200).sendFile(getFile("index.html")) );
+app.route("/test").get(async (req, res) => res.status(200).sendFile(getFile("test.html")) );
+app.route("/api/arduino").get(async (req, res) => {
+    const result = broadcast(req.app.locals.clients1, API_TURNON_LED_RETURN);   ///api/arduino requested, turn on the lamp
+    res.status(200).send('arduino requested ' + (result ? 'sent' : 'not sent!'));
+    console.log('arduino requested ' + (result ? 'sent' : 'not sent!'));
+} );
+
+app.route("/auth").all(function (req, res) {
+    const streamkey = req.body.key;   /* This server is only available to nginx */
+  //  if (streamkey === "supersecret") return res.status(200).send(); /* You can make a database of users instead :) */
+   // return res.status(403).send(); /* Reject the stream */
+   return res.status(200).send();
+  });
+
+
+app.route("*").all((req, res) => res.status(404).send("fail " + req.path) );
+
+
+server.listen(PORT, () => console.log(`${(new Date().toISOString())} Server is listening on port ${PORT}, (Express + WS)`));
+
+
+
+
+
+//web socket:
 wsServer1.on('connection', (socket) => {
     console.log("A client just connected, total clients: " + wsServer1.clients.size);
     socket.on('message', (msg) => {
@@ -43,16 +70,3 @@ wsServer1.on('connection', (socket) => {
 } );
 
 
-app.route("/").get(async (req, res) => res.status(200).sendFile(getFile("index.html")) );
-app.use("/", express.static(getFile()));  //global public folder
-
-app.route("/api/arduino").get(async (req, res) => {
-    const result = broadcast(req.app.locals.clients1, API_TURNON_LED_RETURN);   ///api/arduino requested, turn on the lamp
-    res.status(200).send('arduino requested ' + (result ? 'sent' : 'not sent!'));
-    console.log('arduino requested ' + (result ? 'sent' : 'not sent!'));
-} );
-
-app.route("*").all((req, res) => res.status(404).send("fail " + req.path) );
-
-
-server.listen(PORT, () => console.log(`${(new Date().toISOString())} Server is listening on port ${PORT}, (Express + WS)`));
