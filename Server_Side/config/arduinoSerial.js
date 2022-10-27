0f26f8cd684484eea51e1794d9520eb5ea919794
w3arthur <w3arthur@gmail.com>
2022-10-27 05:14:35 +0300

    refactoring

--- Server_Side/config/arduinoSerial.js
+++ Server_Side/config/arduinoSerial.js
@@ -1,9 +1,7 @@
 const arduinoSerialValueMaxLength = 20;
 const lastSentDisplayTextShow = 10;
-const apiSerialArduinoLedNumber = '7';
 
 module.exports = {
     arduinoSerialValueMaxLength
     , lastSentDisplayTextShow
-    , apiSerialArduinoLedNumber
 };
