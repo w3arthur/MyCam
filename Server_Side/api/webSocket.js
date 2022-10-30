4dab3d95f3a7644b47d6ddeaf3ca5bd6bd18eb86
w3arthur <w3arthur@gmail.com>
2022-10-30 20:46:41 +0200

    refactoring

--- Server_Side/api/webSocket.js
+++ Server_Side/api/webSocket.js
@@ -1,9 +1,11 @@
-const config = require('../config');
-const {webSocketPath1, webSocketPath1ApprovedMessage} = config.webSocket;
 
 const webSocketBroadcast = (clients, message) => {
-    try{ if(!clients || clients.size == 0) throw new Error();
-        clients.forEach((client) => { if (client.readyState === WebSocket.OPEN)  client.send(message); });
+    try{ 
+        if(!clients || clients.size == 0) throw new Error();
+        clients.forEach((client) => { 
+            if (client.readyState === WebSocket.OPEN)  client.send(message);
+            else throw new Error();
+        });
         return true;
     } catch {return false;}
 }
