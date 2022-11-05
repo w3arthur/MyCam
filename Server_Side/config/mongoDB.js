381fe4f427d22b925233755b32acb28ec970fba1
w3arthur <w3arthur@gmail.com>
2022-11-05 19:43:17 +0200

    update

--- Server_Side/config/mongoDB.js
+++ Server_Side/config/mongoDB.js
@@ -1,3 +1,3 @@
-const mongoDBConnectionString = 'mongodb+srv://legopart:WfHIGKcxMGsllNS4@cluster0.uwlwx.mongodb.net/';//'mongodb://localhost:27017/' + 'ArthurCam'; //'mongodb+srv://legopart:WfHIGKcxMGsllNS4@cluster0.uwlwx.mongodb.net/' + 'ArthurCam';
+const mongoDBConnectionString = 'mongodb+srv://legopart:WfHIGKcxMGsllNS4@cluster0.uwlwx.mongodb.net/'+'test';//'mongodb://localhost:27017/' + 'ArthurCam'; //'mongodb+srv://legopart:WfHIGKcxMGsllNS4@cluster0.uwlwx.mongodb.net/' + 'ArthurCam';
 
 module.exports = { mongoDBConnectionString };
--- Server_Side/public_folder/javascript/htmlEvents.js
+++ Server_Side/public_folder/javascript/htmlEvents.js
@@ -17,123 +17,140 @@ finishLoaded(() => {//global page event functions
     callStringsIOTGetApi();
     callCommentsGetApi();
 
-    clickEvent(tags.reload, () => { location.reload(); });
+    if(tags.reload)
+        clickEvent(tags.reload, () => { location.reload(); });
 
-    clickEvent(tags.documentation, () => {  //to fix
-        window.open("https://github.com/w3arthur/ArthurCam.com");
-     });
+    if(tags.documentation)
+        clickEvent(tags.documentation, () => {  //to fix
+            window.open("https://github.com/w3arthur/ArthurCam.com");
+        });
+    if(tags.send_text_button && tags.send_text)
+        clickEvent(tags.send_text_button, () => {
+            let str = tags.send_text.value;
+            if(str.length > configurations.STRING_MAX_LENGTH) {str = str.substring(0, configurations.STRING_MAX_LENGTH)}
+            if(str.trim() === '') return;
+            new AxiosRequest(() => configurations.stringSendApi(str))
+            .GoodResult( (resultData) => { 
+                feedbackAreaText('Waiting, message sending...');
+                delayText(tags.send_text_button, tags.send_text, () => { 
+                    feedbackAreaText('Message is sent', colors.success);
+                    callStringsGetApi();
+                } );
+            } )
+            .BadResult( (errorMsg) => { feedbackAreaText('Cant send the string', colors.fail); } )
+            .Builder();
+        });
+    if(tags.send_iot_text_button && tags.send_iot_text)
+        clickEvent(tags.send_iot_text_button, () => {
+            let str = tags.send_iot_text.value;
+            if(str.length > configurations.STRING_IOT_MAX_LENGTH) {str = str.substring(0, configurations.STRING_IOT_MAX_LENGTH)}
+            if(str.trim() === '') return;
+            new AxiosRequest(() => configurations.stringSendIOTApi(str))
+            .GoodResult( (resultData) => { 
+                feedbackAreaIOTText('Waiting, message sending...');
+                delayText(tags.send_iot_text_button, tags.send_iot_text, () => { 
+                    feedbackAreaIOTText('Message is sent', colors.success);
+                    callStringsIOTGetApi();
+                } );
+            } )
+            .BadResult( (errorMsg) => { feedbackAreaIOTText('Cant send the string', colors.fail); } )
+            .Builder();
+        });
+    if(tags.led)
+        clickEvent(tags.led, () => {
+            new AxiosRequest(() => configurations.ledOnOff())
+            .GoodResult( (resultData) => { feedbackAreaLight('Waiting, sending the led action'); delayButton(tags.led, () => feedbackAreaLight('The request sent', colors.success)); } )
+            .BadResult( (errorMsg) => { feedbackAreaLight('Cant send the led action', colors.fail); } )
+            .Builder();
+        });
 
-    clickEvent(tags.send_text_button, () => {
-        let str = tags.send_text.value;
-        if(str.length > configurations.STRING_MAX_LENGTH) {str = str.substring(0, configurations.STRING_MAX_LENGTH)}
-        if(str.trim() === '') return;
-        new AxiosRequest(() => configurations.stringSendApi(str))
-        .GoodResult( (resultData) => { 
-            feedbackAreaText('Waiting, message sending...');
-            delayText(tags.send_text_button, tags.send_text, () => { 
-                feedbackAreaText('Message is sent', colors.success);
-                callStringsGetApi();
-            } );
-        } )
-        .BadResult( (errorMsg) => { feedbackAreaText('Cant send the string', colors.fail); } )
-        .Builder();
-    });
+    if(tags.lamp)
+        clickEvent(tags.lamp, () => {
+            new AxiosRequest(() => configurations.lampOnOff())
+            .GoodResult( (resultData) => { feedbackAreaLight('Waiting, sending the lamp action'); delayButton(tags.lamp, () => feedbackAreaLight('The request sent', colors.success)); } )
+            .BadResult( (errorMsg) => { feedbackAreaLight('Cant send the lamp action', colors.fail); } )
+            .Builder();
+        });
 
-    clickEvent(tags.send_iot_text_button, () => {
-        let str = tags.send_iot_text.value;
-        if(str.length > configurations.STRING_IOT_MAX_LENGTH) {str = str.substring(0, configurations.STRING_IOT_MAX_LENGTH)}
-        if(str.trim() === '') return;
-        new AxiosRequest(() => configurations.stringSendIOTApi(str))
-        .GoodResult( (resultData) => { 
-            feedbackAreaIOTText('Waiting, message sending...');
-            delayText(tags.send_iot_text_button, tags.send_iot_text, () => { 
-                feedbackAreaIOTText('Message is sent', colors.success);
-                callStringsIOTGetApi();
-            } );
-        } )
-        .BadResult( (errorMsg) => { feedbackAreaIOTText('Cant send the string', colors.fail); } )
-        .Builder();
-    });
-
-    clickEvent(tags.led, () => {
-        new AxiosRequest(() => configurations.ledOnOff())
-        .GoodResult( (resultData) => { feedbackAreaLight('Waiting, sending the led action'); delayButton(tags.led, () => feedbackAreaLight('The request sent', colors.success)); } )
-        .BadResult( (errorMsg) => { feedbackAreaLight('Cant send the led action', colors.fail); } )
-        .Builder();
-    });
-
-    clickEvent(tags.lamp, () => {
-        new AxiosRequest(() => configurations.lampOnOff())
-        .GoodResult( (resultData) => { feedbackAreaLight('Waiting, sending the lamp action'); delayButton(tags.lamp, () => feedbackAreaLight('The request sent', colors.success)); } )
-        .BadResult( (errorMsg) => { feedbackAreaLight('Cant send the lamp action', colors.fail); } )
-        .Builder();
-    });
-
-    clickEvent(tags.send_comment_button, () => {
-        let str = tags.send_comment.value;
-        if(str.length > configurations.COMMENT_MAX_LENGTH) {str = str.substring(0, configurations.COMMENT_MAX_LENGTH);}
-        if(str.trim() === '') return;
-        new AxiosRequest(() => configurations.commentPostApi(str))
-        .GoodResult( (resultData) => {
-            tags.send_comment.value = '';
-            feedbackAreaComment('Comment message is sent, thanks', colors.success);
-            callCommentsGetApi();
-        } )
-        .BadResult( (errorMsg) => { feedbackAreaComment('Sorry cant send the comment', colors.fail); } )
-        .Builder();
-    });
+    if(tags.send_comment_button && tags.send_comment)
+        clickEvent(tags.send_comment_button, () => {
+            let str = tags.send_comment.value;
+            if(str.length > configurations.COMMENT_MAX_LENGTH) {str = str.substring(0, configurations.COMMENT_MAX_LENGTH);}
+            if(str.trim() === '') return;
+            new AxiosRequest(() => configurations.commentPostApi(str))
+            .GoodResult( (resultData) => {
+                tags.send_comment.value = '';
+                feedbackAreaComment('Comment message is sent, thanks', colors.success);
+                callCommentsGetApi();
+            } )
+            .BadResult( (errorMsg) => { feedbackAreaComment('Sorry cant send the comment', colors.fail); } )
+            .Builder();
+        });
 
 });
 
 
 //LED or Lamp
 function feedbackAreaLight(str, color = colors.process){
+    if(tags.feedbackAreaLight_element)
         feedbackArea(tags.feedbackAreaLight_element ,str, color); 
 }
 
 //String Serial TextArea
-function feedbackAreaText(str, color = colors.process){ feedbackArea(tags.feedbackAreaText_element ,str, color); }
+function feedbackAreaText(str, color = colors.process){ 
+    if(tags.feedbackAreaText_element)
+        feedbackArea(tags.feedbackAreaText_element ,str, color); 
+}
 
 
 //Comment Area
-function feedbackAreaComment(str, color = colors.process){ feedbackArea(tags.feedbackAreaComment_element ,str, color); }
+function feedbackAreaComment(str, color = colors.process){
+    if(tags.feedbackAreaComment_element)
+        feedbackArea(tags.feedbackAreaComment_element ,str, color); 
+}
 
 //String IOT TextArea
-function feedbackAreaIOTText(str, color = colors.process){ feedbackArea(tags.feedbackAreaIOTText_element ,str, color); }
+function feedbackAreaIOTText(str, color = colors.process){
+    if(tags.feedbackAreaIOTText_element)
+        feedbackArea(tags.feedbackAreaIOTText_element ,str, color);
+}
 
 
 function callStringsGetApi(){
-    new AxiosRequest(() => configurations.stringsGetApi())
-        .GoodResult( (resultData) => { 
-            let str = '';
-            try{ resultData.displayText.map( x => { str += ' -' + x; }); } 
-            catch(e){ throw new Error(); }
-            tags.displayedText.textContent = str;
-        } )
-        .BadResult( (errorMsg) => { tags.displayedText.textContent = '*error load data'; tags.displayedText.className = 'red'; } )
-        .Builder();
+    if(tags.displayedText)
+        new AxiosRequest(() => configurations.stringsGetApi())
+            .GoodResult( (resultData) => { 
+                let str = '';
+                try{ resultData.displayText.map( x => { str += ' -' + x; }); } 
+                catch(e){ throw new Error(); }
+                tags.displayedText.textContent = str;
+            } )
+            .BadResult( (errorMsg) => { tags.displayedText.textContent = '*error load data'; tags.displayedText.className = 'red'; } )
+            .Builder();
 }
 
 function callStringsIOTGetApi(){
-    new AxiosRequest(() => configurations.stringsIOTGetApi())
-        .GoodResult( (resultData) => { 
-            let str = '';
-            try{ resultData.displayText.map( x => { str += ' -' + x; }); } 
-            catch(e){ throw new Error(); }
-            tags.displayedIOTText.textContent = str;
-        } )
-        .BadResult( (errorMsg) => { tags.displayedIOTText.textContent = '*error load data'; tags.displayedIOTText.className = 'red'; } )
-        .Builder();
+    if(tags.displayedIOTText)
+        new AxiosRequest(() => configurations.stringsIOTGetApi())
+            .GoodResult( (resultData) => { 
+                let str = '';
+                try{ resultData.displayText.map( x => { str += ' -' + x; }); } 
+                catch(e){ throw new Error(); }
+                tags.displayedIOTText.textContent = str;
+            } )
+            .BadResult( (errorMsg) => { tags.displayedIOTText.textContent = '*error load data'; tags.displayedIOTText.className = 'red'; } )
+            .Builder();
 }
 
 function callCommentsGetApi(){
-    new AxiosRequest(() => configurations.commentsGetApi())
-        .GoodResult( (resultData) => { 
-            let str = '';
-            try{ resultData.comments.map( x => { str += '-' + x.replaceAll('<', '&lt;') + "<br>"; }); } 
-            catch(e){ throw new Error(); }
-            tags.comments.innerHTML = str;
-        } )
-        .BadResult( (errorMsg) => { tags.comments.textContent = '*error load data'; tags.comments.className = 'red'; } )
-        .Builder();
+    if(tags.comments)
+        new AxiosRequest(() => configurations.commentsGetApi())
+            .GoodResult( (resultData) => { 
+                let str = '';
+                try{ resultData.comments.map( x => { str += '-' + x.replaceAll('<', '&lt;') + "<br>"; }); } 
+                catch(e){ throw new Error(); }
+                tags.comments.innerHTML = str;
+            } )
+            .BadResult( (errorMsg) => { tags.comments.textContent = '*error load data'; tags.comments.className = 'red'; } )
+            .Builder();
 }
