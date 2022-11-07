3fa256db27625d2c2f7e002b13bec3559d87881b
w3arthur <w3arthur@gmail.com>
2022-11-07 23:45:21 +0200

    refactoring

--- ArdoinoSerial/SerialReadArduino_with_sh1106_string/SerialReadArduino_with_sh1106_string.ino
+++ ArdoinoSerial/SerialReadArduino_with_sh1106_string/SerialReadArduino_with_sh1106_string.ino
@@ -13,8 +13,12 @@
 #define RESET_STRING_ACTION "Your Text Here->"
 #define RESET_SERIAL_INPUT_VALUE 42
 #define STRING_SERIAL_INPUT_VALUE 9
-#define LED1 7
+#define LED1_2 7
+#define LAMP 2
+
 #define LED1_VALUE 7
+#define LED2_VALUE 8
+#define LAMP_VALUE 2
 
 //U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);  // I2C / TWI 
 //U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_FAST); // Dev 0, Fast I2C / TWI
@@ -22,8 +26,16 @@ U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NO_ACK); // Display which does not send ACK
 
 
 String userString = RESET_STRING;
+bool isLedPlay = false;
 
-int second = 1;
+void ledPlay(){
+          digitalWrite(LED1_VALUE, !digitalRead(LED1_VALUE)); 
+          digitalWrite(LED2_VALUE, !digitalRead(LED1_VALUE)); 
+          delay(1000);
+}
+
+
+int second = 0; //second counter
 
 void draw(void) {
   //u8g.setFont(u8g_font_unifont);  // graphic commands to redraw the complete screen should be placed here  
@@ -43,9 +55,11 @@ void draw(void) {
 
 
 void setup(void) {
-  pinMode(LED1, OUTPUT);
+  pinMode(LAMP_VALUE, OUTPUT);
+  pinMode(LED1_VALUE, OUTPUT);
+  pinMode(LED2_VALUE, OUTPUT);
   Serial.begin(9600);
-  Serial.println("Waiting to true or 1 value... ");
+  Serial.println("Waiting to "+ String(STRING_SERIAL_INPUT_VALUE) +" and string or "+ String(LED1_2) +" value... ");
   u8g.setRot180(); // flip screen, if required
   //u8g.setHardwareBackup(u8g_backup_avr_spi);  // set SPI backup if required
   // assign default color value:
@@ -59,10 +73,11 @@ void loop(void) {
   u8g_prepare();
   u8g.firstPage();  
   do { draw(); } while( u8g.nextPage() );  // picture loop
-  //do in parralel
-  second %= 59;
-  ++second;
-  delay(1000); // rebuild the picture after some delay
+  
+  
+  second = (millis()/1000)%60;
+  if(isLedPlay) ledPlay();
+  else delay(1000); // rebuild the picture after some delay
 }
 
 
@@ -71,7 +86,15 @@ void serialEvent()
 {
     while (!Serial.available());
     int action = Serial.parseInt();
-    if ( action == LED1_VALUE ){ digitalWrite(LED1, !digitalRead(LED1)); Serial.println("ok");}
+    if ( action == LED1_2 ){
+      isLedPlay = !isLedPlay;
+      if(!isLedPlay){
+        digitalWrite(LED1_VALUE, LOW); 
+        digitalWrite(LED2_VALUE, LOW); 
+      }
+      Serial.println("ok led");
+    }
+    else if ( action == LAMP ){ digitalWrite(LAMP_VALUE, !digitalRead(LAMP_VALUE)); Serial.println("ok lamp");}
     else if ( action == STRING_SERIAL_INPUT_VALUE ){
         String diplayString = "";
         Serial.println("waiting for string, waiting set for (msec): " + String(TIME_DELAY_FOR_STRING_INPUT));
@@ -84,16 +107,16 @@ void serialEvent()
         if(diplayString != ""){
           userString = diplayString;
           Serial.println(diplayString);
-          Serial.println("ok");
+          Serial.println("ok string");
         }
         else Serial.println("-");
     }
     else if ( action == RESET_SERIAL_INPUT_VALUE ){
       userString = RESET_STRING_ACTION;
       for(int i = 0; i < 2; ++i ){
-          digitalWrite(LED1, HIGH); 
+          digitalWrite(LED1_VALUE, !digitalRead(LED1_VALUE)); 
           delay(500);
-          digitalWrite(LED1, LOW); 
+          digitalWrite(LED1_VALUE, !digitalRead(LED1_VALUE)); 
           delay(500);
       }
       Serial.println("arduino restart");
